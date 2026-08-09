144edcfa8976d996773bb1310fe687eee5e31805
devansh30081 <bhuwalkadevansh@gmail.com>
2026-08-09 15:24:27 +0530

    Time: 2 ms (19.92%) | Memory: 8.5 MB (51.17%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int ans=0;
+       int low=1; int high=x;
+       while(low<=high){
+        long long mid=low+(high-low)/2;
+        if(mid*mid<=x){
+            ans=mid;
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+       }
+       return ans;
+    }
+};
