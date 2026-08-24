8a2406f0784a9788bf59d3b5eef00b21f0807493
devansh30081 <bhuwalkadevansh@gmail.com>
2026-08-24 16:36:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (93.33%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double answer(double x, long long n) {
+        if (n == 0)
+            return 1;
+
+        double half = answer(x, n / 2);
+
+        if (n % 2 == 0)
+            return half * half;
+        else
+            return x * half * half;
+    }
+
+    double myPow(double x, int n) {
+        long long N = n;
+
+        if (N < 0)
+            return 1.0 / answer(x, -N);
+
+        return answer(x, N);
+    }
+};
