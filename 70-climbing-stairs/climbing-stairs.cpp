6cd801e7ec98804e95686ea858b13716dc4f94f4
devansh30081 <bhuwalkadevansh@gmail.com>
2026-08-29 12:44:16 +0530

    Time: 2 ms (5.10%) | Memory: 8.7 MB (20.89%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fib(int n, vector<int>& dp) {
+        if (n <= 1)
+            return n;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        return dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n + 2, -1);
+        return fib(n + 1, dp);
+    }
+};
